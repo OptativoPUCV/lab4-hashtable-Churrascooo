2e66fbe6d8f20c98f2acc58d9150d0eb4bc5aab4
Churrascooo <163452323+Churrascooo@users.noreply.github.com>
2024-04-23 18:44:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,7 +39,9 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
+void insertMap(HashMap * map, char * key, void * value)
+{
+    
 
 
 }
@@ -65,9 +67,17 @@ HashMap * createMap(long capacity)
     return NULL;
 }
 
-void eraseMap(HashMap * map,  char * key) {    
-
-
+void eraseMap(HashMap * map,  char * key)
+{
+  long pos = hash(key, map->capacity);
+  if (map->buckets[pos] != NULL)
+  {
+    if (strcmp(map->buckets[pos]->key, key))
+    {
+      map->buckets[pos] = NULL;
+      map->size--;
+    }
+  }
 }
 
 Pair * searchMap(HashMap * map,  char * key)
